56717186e90def00537db77dda2ef7cc370c982f
Illnoid <stefan2846@hotmail.com>
2018-11-05 13:20:30 +0100

    Changed the parameter and return types from Int to Integer

--- FunkProg/lab1.hs
+++ FunkProg/lab1.hs
@@ -6,11 +6,11 @@ power n k = n * power n (k - 1)
 
 -- listOfN is produced by list comprehension where the list size is determined
 -- by k and the list content by n
-power1 :: Int -> Int -> Int
+power1 :: Integer -> Integer -> Integer
 power1 n k | k < 0 = error "power : negative argument"
 power1 n 0 = 1
 power1 n k = product listOfN
-    where listOfN = take k [n,n..]
+    where listOfN = take (fromInteger k) [n,n..]
 
 -- Usage of guard cases in order to run the right calculation depending on k
 power2 :: Integer -> Integer -> Integer
