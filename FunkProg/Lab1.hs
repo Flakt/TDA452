558c9bb6ab515d6f51b4bd08fbaf449c819a3bd9
Magnus Wamby <wamby@student.chalmers.se>
2018-11-07 23:43:28 +0100

    Update based on feedback
    changed a from `n+1` to `k+1`
    
    removed the case for `k = 0` since `product []` returns 0
    
    changed `power 2` to use itself, which should probably work out of the bo

--- FunkProg/Lab1.hs
+++ FunkProg/Lab1.hs
@@ -1,6 +1,6 @@
 import Test.QuickCheck
 
--- The amount of steps of power will always be (n + 1) step
+-- The amount of steps of power will always be (k + 1) step
 power :: Integer -> Integer -> Integer
 power n k | k < 0 = error "power : negative argument"
 power n 0 = 1
@@ -11,19 +11,18 @@ power n k = n * power n (k - 1)
     where the list size is determined
     by k and the list content by n -}
 power1 :: Integer -> Integer -> Integer
-power1 n k | k < 0 = error "power : negative argument"
-power1 n 0 = 1
+power1 n k | k < 0 = error "power : negative argument" 
 power1 n k = product listOfN
     where listOfN = take (fromInteger k) [n,n..]
 
 -- Usage of guard cases in order to run the right calculation depending if k
 -- is odd or even
 power2 :: Integer -> Integer -> Integer
-power2 n k | k < 0 = error "power : negative argument"
+power2 n k | k < 0 = error "power : negative argument" 
 power2 n 0 = 1
 power2 n k
-    | even k = (n * n)^(k `div` 2)
-    | odd k = n * (n^(k - 1))
+    | even k = power2 (n * n) (k `div` 2)
+    | odd k = n * power2 n (k-1)
 
 -- A
 
