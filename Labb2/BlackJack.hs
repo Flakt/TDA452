c10662076e0006d03f494b338e5c1b63f836688c
magnus <magnus.lwamby@gmail.com>
2018-11-10 14:29:43 +0100

    Finish A

--- Labb2/BlackJack.hs
+++ Labb2/BlackJack.hs
@@ -1,37 +1,52 @@
 module BlackJack where
 import Cards
 import RunGame
+import Test.QuickCheck
 
+{-
+3.1 A0
 
--- An empty hand (of size 0)
-empty :: Hand
-empty = Empty
+size hand2 
+    = size (Add (Card (Numeric 2) Hearts) 
+        (Add (Card Jack Spades) Empty))
+    = 1 + size (Add (Card Jack Spades) Empty)
+    = 1 + 1 + size Empty 
+    = 1 + 1 + 0
+    = 2
+-}
 
--- Returns the value of a rank
+-- Returns the winners (aces = 11 or 1 if needed)
+winner :: Hand -> Hand -> Player
+winner guest bank 
+    | valueHandBest guest > valueHandBest bank = Guest
+    | otherwise = Bank
+
+-- Returns the best possible value of a hand (ace = 11 or 1  if needed)
+valueHandBest :: Hand -> Integer
+valueHandBest hand 
+    | valueHand hand > 21 = valueAceValue hand 1
+    | otherwise           = valueHand hand            
+
+-- Returns the value of a hand (ace = 11)
+valueHand :: Hand -> Integer
+valueHand Empty = 0
+valueHand (Add card hand) = valueCard card + valueHand hand
+
+-- Returns the value of a card (ace = 11)
+valueCard :: Card -> Integer
+valueCard card = valueRank (rank card)
+
+-- Returns the value of a rank (ace = 11)
 valueRank :: Rank -> Integer
--- valueRank rank | rank `elem` [Jack,Queen,King] = 10 -- alternative implementation
 valueRank Jack       = 10
 valueRank Queen      = 10
 valueRank King       = 10
 valueRank Ace        = 11
-valueRank Numeric n  = n
+valueRank (Numeric n)  = n
 
--- Returns the value of a suit (for tie breakers)
--- could also be done by having Suit derive from Ord
-valueSuit :: Suit -> Integer
-valueSuit Heart     = 4
-valueSuit Spades    = 3
-valueSuit Diamonds  = 2
-valueSuit Clubs     = 1
-
--- Returns the value of a hand (without smart aces)
-value :: Hand -> Integer
-value Empty = 0
-value (Add card hand) = valueOf (rank card) + value hand
-
--- Returns the value of a hand where the value of aces is replaced value 
+-- Returns the value of a hand, replacing the value of aces (ace = n) 
 valueAceValue :: Hand -> Integer -> Integer
-valueAceValue hand value = value hand -(aces * 11) + aces * value 
+valueAceValue hand value = valueHand hand -(aces * 11) + aces * value 
     where 
         aces = countAces hand 
 
@@ -42,17 +57,37 @@ countAces (Add card hand)
         | rank card == Ace = 1 + countAces hand
         | otherwise        = countAces hand
 
+-- Helper function for generating a hand of size n of only ace of hearts
+handAces :: Integer -> Hand
+handAces n  | n <= 0 = Empty
+            | otherwise = Add (Card Ace Hearts) (handAces (n-1))    
+
+-- Not used in A
+
+-- Returns true if the hand is bust (aces = 11 or 1  if needed)
+gameOver :: Hand -> Bool
+gameOver hand = valueHand hand > 21 &&
+                valueAceValue hand 1 > 21
+
+-- An empty hand (of size 0)
+empty :: Hand
+empty = Empty
+
+------------------------------Tests------------------------------------------
+
+card10 = Card (Numeric 10) Hearts
+card2 = Card (Numeric 2) Hearts
+cardAce = Card Ace Hearts
+cardJack = Card Jack Hearts
+
+hand10 = Add card10 Empty       
+hand12 = Add card2 Empty
+hand22 = Add card10 (Add card10 (Add card2 Empty)) -- should always bust
+hand32or12 = Add cardAce (Add cardAce (Add card10 Empty))
+hand22or2 = Add cardAce (Add cardAce Empty)
+hand21 = Add cardAce (Add cardJack Empty)
+
 -- Tests that a hand of n aces counts n aces
 propCountAces :: Integer -> Bool
 propCountAces n =
-        countAces(hand n) == n
-        where 
-            hand n 
-                | n <= 0 = Empty
-                | otherwise = Add Card Ace Hearts hand (n-1)
-
--- Returns true if the hand is bust (with smart aces)
-gameOver :: Hand -> Bool
-
--- Returns the winners (1st parameter is player's hand)
-winner :: Hand -> Hand -> Player
+    countAces(handAces (abs n)) == abs n
