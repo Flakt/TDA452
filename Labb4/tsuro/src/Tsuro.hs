1c76740fb6819c7c8547d959b0c835e528d4c6ac
EmElw <magnus.lwamby@gmail.com>
2018-12-17 17:53:14 +0100

    make it compile, add a rudimentary show implementation

--- Labb4/tsuro/src/Tsuro.hs
+++ Labb4/tsuro/src/Tsuro.hs
@@ -5,19 +5,25 @@ import Data.Maybe
 -- TODO evaluate if a Map is more useful in some cases
 
 -- Magic constants for board size
-bW = 6      -- board width
-q = bW - 1  -- max board index
+bw = 6      -- board width
+q = bw - 1  -- max board index
 
 -- | The entire game state, including players
 data Game = Game {players :: Player, board :: Board, deck :: [Tile]}
 
 -- | The board, a list of rows of tiles, standard size is 6x6
-data Board = Board {tiles :: [[Maybe Tile]], pieces :: [Piece]}
+data Board = Board {tiles :: [[Maybe Tile]]}
+emptyBoard = Board (replicate bw (replicate bw Nothing))
+instance Show Board where
+    show b = concat lines
+        where 
+            lines = map (++"\n") $ map f (tiles b)
+            f = concatMap (maybe "." (\x -> "T"))
 
 -- | A single tile, which
 newtype Tile = Tile {conn :: [Connection]}
     deriving (Eq, Show)
-
+    
 {-  Connections are internal within the Tile
     Below is a representation of how the Links are indexed
     Rotating a tile is done by transposing its connections
@@ -30,8 +36,10 @@ newtype Tile = Tile {conn :: [Connection]}
 type Connection = (Link, Link)
 type Link = Int
 
-data Player = Player {playerId :: ID, hand :: [Tile], pos :: Maybe Pos,
-                      link :: Maybe Link}
+-- | A player, represented by an id, a hand (which is a list of tiles)
+-- and a starting position
+data Player = Player {playerId :: ID, hand :: [Tile], pos :: Pos,
+                      link :: Link}
 type ID = Int
 
 -- A list of tiles (strictly speaking it is unordered)
@@ -39,62 +47,54 @@ type Hand = [Tile]
 
 type Pos = (Int, Int)
 
--- Updates the board
+-- Updates the board by placing the tile on the given position
 -- * causes pieces to slide
 updateBoard :: Board -> Pos -> Tile -> Board
-updateBoard b p t = Board new_tiles new_players
-    where
-        new_players = undefined -- TODO
-        new_tiles = updateTile (tiles b) p t
+updateBoard b p t = Board (updateTile (tiles b) p t)
 
--- | Simulates the movement of a player piece (moves it forward until it reaches
---  a bare connection or collides with an another player) * recursively
-movePlayer :: Board -> Pos -> Link -> (Tile, Connection)
-movePlayer b p l | posOutOfBounds p = (tile, connection)
-movePlayer b p l | isNothing next_tile = (tile, connection)
-movePlayer b p l = movePlayer b newPos newLink
+-- | Simulates the movement of a player piece (moves it forward until it 
+-- reaches a bare connection or collides with an another player) and returns 
+-- the final position, tile and connection
+movePlayer :: Board -> Pos -> Link -> (Pos, Maybe Tile, Connection)
+movePlayer b p l | isOutside newPos = (newPos, tile, connection)
+                 | isNothing tile   = (newPos, tile, connection)
+                 | otherwise        =  movePlayer b newPos newLink
     where
       tile       = b @@ p
-      connection = getConnection l tile
+      connection = findConnection l (fromJust tile)
       newPos     = p +++ linkOffs l
       newLink    = mapLinks l
 
-
--- | Gets the appropiate connection based on the given link
-getConnection :: Link -> Tile -> Connection
-getConnection l t = map f (conn tile)
+-- | Returns the connection in the given tile containing the given link
+findConnection :: Link -> Tile -> Connection
+findConnection l t = fromJust(find f (conn t))
     where f (a, b) = a == target || b == target
           target   = mapLinks l
 
 -- | Checks if a given pos is out of the game boundaries
-posOutOfBounds :: Pos -> Bool
-posOutOfBounds pos = (x > bw || y > bw) || (x < 0 || y < 0)
+isOutside :: Pos -> Bool
+isOutside pos = (x > bw || y > bw) || (x < 0 || y < 0)
     where
       x = fst pos
       y = snd pos
 
 -- | Maps links with corresponding link of the other tile
 mapLinks :: Link -> Link
-mapLinks 0 = 5
-mapLinks 1 = 4
-mapLinks 2 = 7
-mapLinks 3 = 6
-mapLinks 4 = 1
-mapLinks 5 = 0
-mapLinks 6 = 3
-mapLinks 7 = 2
-
--- Returns Just the next tile to travel to, or Nothing
+mapLinks l | even l = (l + 5) `mod` 8
+mapLinks l | odd  l = (l + 3) `mod` 8
+ 
+-- | Returns Just the next tile to travel to, or Nothing
 nextTile :: Board -> Pos -> Link -> Maybe Tile
 nextTile b p l = b @@ p'
     where
         p' = p +++ linkOffs l
 
--- "board @@ position" returns the tile (if any) at the position
+-- | Returns the tile, or Nothing
+-- Will throw index errors if given bad pos
 (@@) :: Board -> Pos -> Maybe Tile
 (@@) b (x,y) = (tiles b !! x) !! y
 
--- Returns the offset in position from a given link
+-- | Returns the offset in position from a given link
 linkOffs :: Link -> (Int, Int)
 linkOffs l  | l == 0 || l == 1 = ( 0,-1)
             | l == 2 || l == 3 = ( 1, 0)
@@ -125,11 +125,7 @@ adjacentPos (a,b) = zip [  a,a+1,  a,a-1]
 -- returns Nothing if the player doesn't have a piece
 -- TODO should maybe consider that the position is on the board
 legalPos :: Board -> Player -> Maybe Pos
-legalPos b p = if isJust piece then Just pos' else Nothing
-    where
-        pos' = fromJust (pos piece') +++ linkOffs  (fromJust (link piece'))
-        piece' = fromJust piece
-        piece = find (\x -> pieceId x == playerId p) (pieces b)
+legalPos b p = undefined -- TODO
 
 -- | Position addition
 (+++) :: Pos -> Pos -> Pos
@@ -141,6 +137,6 @@ rotateTile t = Tile (map transposeConn (conn t))
     where transposeConn (a,b) = (transposeLink a,transposeLink b)
           transposeLink x = (x +2) `mod` 8
 
-detectCollision :: Board -> Player -> Player -> Bool
-detectCollision b p1 p2 = samePosAndLink
-    where samePosAndLink = pos p1 && pos p2 && link p1 && link p2
+--detectCollision :: Board -> Player -> Player -> Bool
+--detectCollision b p1 p2 = samePosAndLink
+--    where samePosAndLink = pos p1 && pos p2 && link p1 && link p2
