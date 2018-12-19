c9c7482a93ae8e31cb2728156d5d5add38e416e1
EmElw <magnus.lwamby@gmail.com>
2018-12-19 07:19:36 +0100

    more work

--- Labb4/tsuro/app/Main.hs
+++ Labb4/tsuro/app/Main.hs
@@ -16,24 +16,28 @@ main :: IO ()
 main = do
     void initGUI
     window <- windowNew
-    set window [ windowTitle         := "Calculator"
+    set window [ windowTitle         := "Tsuro"
                , windowResizable     := False]
     
-    --let (deck,_) = shuffle (mkStdGen 123123) [normalize $ rotateTile (defaultDeck !! 5) 0]
-    --let board = boardFromDeck deck
-    --grid <- displayBoard board 
-
     let game = sampleGame 
 
     gameBox <- displayState game
 
-    containerAdd window gameBox
+    -- root <- getCurrentDirectory
+    -- test <- overlayNew
+    -- img1 <- imageNewFromFile $ root ++ "/assets/blank.png"
+    -- img2 <- imageNewFromFile $ root ++ "/assets/player0.png"
+    -- overlayAdd test img1
+    -- overlayAdd test img2
+    -- img1 <- playerToImage 0 2
+
+    containerAdd window img1
 
     window `on` deleteEvent $ do
         liftIO mainQuit
         return False
 
-    widgetShowAll window
+    widgetShowAll window    
     mainGUI
 
 -- | Creates a new element based on a game state, containing 
@@ -45,7 +49,7 @@ displayState game = do
 
     -- add the board
     vb <- vBoxNew False 10
-    boardGrid <- displayBoard (board game)
+    boardGrid <- displayBoard (board game) (players game)
     boxPackStart vb boardGrid PackNatural 0     
 
     -- add the current player's hand
@@ -55,6 +59,7 @@ displayState game = do
     
     return vb
 
+-- | Renders a hand as a row of tiles
 displayHand :: [Tile] -> IO HBox
 displayHand hand = do
     hb <- hBoxNew True 10
@@ -63,25 +68,43 @@ displayHand hand = do
         boxPackStart hb img PackNatural 0) hand
     return hb
 
-displayBoard :: Board -> IO HBox
-displayBoard b = do
+-- | Renders a board as a grid of tiles with player pieces
+displayBoard :: Board -> [Player] ->IO HBox
+displayBoard b ps = do
     let coords = [(x,y) | y <- [0..5], x <- [0..5]]
     let tiles' = zip (concat $ tiles b) coords
 
     grid <- gridNew
-    mapM_ (attachTile grid) tiles' -- maybe works?
+    gridSetRowHomogeneous grid True
+    gridSetColumnHomogeneous grid True
+    mapM_ (attachTile grid) tiles'
+
+    let positions = undefined -- TODO map (\pl -> movePlayer pl 
 
     hb <- hBoxNew True 0
     boxPackStart hb grid PackNatural 0
     return hb
 
--- | Attaches a tile to a grid
+-- | Attaches a overlay with an image to the grid
 attachTile :: Grid -> (Maybe Tile, Pos) -> IO ()
 attachTile grid (tile, (x,y)) = do
     img <- tileToImage tile 
     gridAttach grid img x y 1 1    
 
+-- | Renders a player's piece
+playerToImage :: Int -> Link -> IO Image -- TODO this can probably be improved
+playerToImage id l 
+    | odd l = do
+        root    <- getCurrentDirectory
+        img     <- imageNewFromFile $ root ++ "/assets/player" ++ show id ++ ".png" 
+        imgFlip <- fliphImage img 
+        rotateImage imgFlip (l `div` 2)
+    | otherwise = do
+        root    <- getCurrentDirectory
+        img     <- imageNewFromFile $ root ++ "/assets/player" ++ show id ++ ".png" 
+        rotateImage img (l `div` 2)
 
+-- | Renders a tile
 tileToImage :: Maybe Tile -> IO Image
 tileToImage Nothing = do
     fp <- tileToFilepath Nothing
@@ -96,7 +119,6 @@ tileToImage (Just tile) = do
 -- | Returns the number of 90 deg clockwise rotations from a base tile 
 rotationsFromBase :: Tile -> Int    
 rotationsFromBase t = rotationsFromBase' t 0
-
 -- works by rotating counter clockwise until it is in the default deck
 rotationsFromBase' tile num 
     | tile `elem` defaultDeck = num
@@ -109,6 +131,13 @@ tileToFilepath tile = do
     let id = maybe "blank" (filter (/=',') . dropFirstAndLast . show . toList . normalize) tile
     return (root ++ "/assets/" ++ 
             id ++ ".png")
+
+-- | Flips an image horizontally (i.e. over x-axis)
+fliphImage :: Image -> IO Image
+fliphImage img = do
+    pb <- imageGetPixbuf img
+    pbflip <- pixbufFlipHorizontally pb
+    imageNewFromPixbuf pbflip
 
 -- | Rotates an image by 90 deg n times clockwise
 rotateImage :: Image -> Int -> IO Image
